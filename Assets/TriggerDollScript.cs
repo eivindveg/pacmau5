30971a175c000a47c3b5b1ae97b09c0fab8cb28b
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-02-25 19:49:21 +0100

    Improve code quality and fix rotation bug.

--- Assets/TriggerDollScript.cs
+++ Assets/TriggerDollScript.cs
@@ -3,63 +3,56 @@
 // ReSharper disable once CheckNamespace
 public class TriggerDollScript : MonoBehaviour
 {
-
-    GameObject _left;
-    GameObject _right;
-    GameObject _front;
     public const float ScanDistance = 1.0f / 15.0f;
-    // Use this for initialization
-// ReSharper disable once UnusedMember.Local
-    void Start()
-    {
-        _left = transform.Find("LeftTrigger").gameObject;
-        _right = transform.Find("RightTrigger").gameObject;
-        _front = transform.Find("FrontTrigger").gameObject;
-    }
-
-    // Update is called once per frame
-// ReSharper disable once UnusedMember.Local
-    void Update()
-    {
-
-    }
+    private GameObject left;
+    private GameObject right;
+    private GameObject front;
 
     public bool IsLeftClear()
     {
-        Vector3 direction = transform.TransformDirection(Vector3.left);
-        if (Physics.Raycast(new Ray(_left.transform.position, direction), (ScanDistance + transform.localScale.x)))
-        {
-            return false;
-        }
-        return true;
-        
+        var direction = transform.TransformDirection(Vector3.left);
+        return !Physics.Raycast(new Ray(this.left.transform.position, direction), ScanDistance + this.transform.localScale.x);
     }
+
     public bool IsRightClear()
     {
         var direction = transform.TransformDirection(Vector3.right);
-        return !Physics.Raycast(new Ray(_right.transform.position, direction), (ScanDistance + transform.localScale.x));
+        return !Physics.Raycast(new Ray(this.right.transform.position, direction), ScanDistance + transform.localScale.x);
     }
 
     public bool IsClearOnBothSides()
     {
-        return (IsLeftClear() && IsRightClear());
+        return this.IsLeftClear() && this.IsRightClear();
     }
+
     public bool IsClearForward()
     {
         var forwardDirection = transform.TransformDirection(Vector3.forward);
 
-        if (Physics.Raycast(new Ray(_front.transform.position, forwardDirection), (ScanDistance)))
+        if (Physics.Raycast(new Ray(this.front.transform.position, forwardDirection), ScanDistance))
         {
             return false;
         }
-        if (Physics.Raycast(new Ray(_left.transform.position, forwardDirection), (ScanDistance + transform.localScale.x)))
+
+        if (Physics.Raycast(new Ray(this.left.transform.position, forwardDirection), ScanDistance + transform.localScale.x))
         {
             return false;
         }
-        if (Physics.Raycast(new Ray(_right.transform.position, forwardDirection), ScanDistance + transform.localScale.x))
+
+        if (Physics.Raycast(new Ray(this.right.transform.position, forwardDirection), ScanDistance + transform.localScale.x))
         {
             return false;
         }
+
         return true;
+    }
+
+    // Use this for initialization
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+        this.left = transform.Find("LeftTrigger").gameObject;
+        this.right = transform.Find("RightTrigger").gameObject;
+        this.front = transform.Find("FrontTrigger").gameObject;
     }
 }
