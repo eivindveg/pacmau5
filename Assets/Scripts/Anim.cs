1989a6463ef62f78e420040b8605d93dba34f190
Runnetty1 <rcdriftnitro2@gmail.com>
2014-02-25 19:51:45 +0100

    Anim

---
+++ Assets/Scripts/Anim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim : MonoBehaviour {
+	public Animator anim;
+	
+	void Update () {
+		//Fix this.
+		if(this.transform.rigidbody.velocity.magnitude > 0.2)
+			anim.SetBool("isMoving",true);
+
+		}else{
+			anim.SetBool("isMoving",false);
+		}
+	
+	}
+}
