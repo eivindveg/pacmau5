5db48729a4a657c45c3592ef8ecd8e1a23a65877
hakonm <hakonfmelby@gmail.com>
2014-02-26 10:43:55 +0100

    tull

---
+++ Assets/Scripts/Anim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+
+public class Anim : MonoBehaviour {
+	public Animator anim;
+	public GameObject obj;
+	static int runState = Animator.StringToHash("isMoving");
+	
+	void Update () {
+		if(obj.rigidbody.IsSleeping()) {
+			anim.SetBool(runState,true);
+		}else{
+			anim.SetBool(runState,false);
+		}
+	}
+}
