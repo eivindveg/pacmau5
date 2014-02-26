0cfc5b93c29d3ad804f93c6bcf4191c335a27f1d
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-02-26 16:25:22 +0100

    Fix conflicts

--- Assets/gameOverButtons.cs
+++ Assets/gameOverButtons.cs
@@ -5,7 +5,10 @@ public class gameOverButtons : MonoBehaviour {
 
 
 	void OnGUI () {
-		
+
+			
+			
+			
 			if (GUI.Button (new Rect (Screen.width / 2 - 80, Screen.height / 2 + 200, 160, 40), "Main Menu")) {
 				Application.LoadLevel ("Start_Menu");
 				
