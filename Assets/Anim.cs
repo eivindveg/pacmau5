8bd18c04a95eaaf21cf2a9f126fb6aa0c4836975
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-02-26 18:57:06 +0100

    Fix player animation prefab

--- Assets/Anim.cs
+++ Assets/Anim.cs
@@ -4,15 +4,16 @@
 
 
 public class Anim : MonoBehaviour {
-	public Animator anim;
-	public GameObject obj;
-	static int runState = Animator.StringToHash("isMoving");
-	
-	void Update () {
-		if(obj.rigidbody.IsSleeping()) {
-			anim.SetBool(runState,true);
-		}else{
-			anim.SetBool(runState,false);
-		}
-	}
+    public Animator anim;
+    public GameObject obj;
+    static int runState = Animator.StringToHash("isMoving");
+    
+    void Update ()
+    {
+        if(obj.rigidbody.IsSleeping()) {
+            this.anim.SetBool(runState,true);
+        }else{
+            this.anim.SetBool(runState,false);
+        }
+    }
 }
