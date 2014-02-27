0df9e041bf8c21981a535002eb7b1887639201bc
hakonm <hakonfmelby@gmail.com>
2014-02-27 19:00:48 +0100

    Added ScoreHUD

---
+++ Assets/ScoreHUDScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreHUDScript : MonoBehaviour {
+
+	public GUIText CurrentScore;
+	public GUIText HighScore;
+	public GUIText LivesLeft;
+
+	// Use this for initialization
+	void Start () {
+		HighScore.text = "HighScore " + ScoreScript.HighScore;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CurrentScore.text = "Current Score: " + ScoreScript.CurrentScore;
+		LivesLeft.text = "Lives: " + ScoreScript.Lives;
+	}
+}
