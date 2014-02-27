fb61d4cf19f8e5d5250cabc7bd3d9a24d64840cf
RushTea <Eivindveg@hotmail.com>
2014-02-27 12:20:31 +0100

    Update GameOverButtons.cs

--- Assets/GameOverButtons.cs
+++ Assets/GameOverButtons.cs
@@ -1,17 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameOverButtons : MonoBehaviour {
-
-
+public class GameOverButtons : MonoBehaviour
+{
 	void OnGUI ()
 	{
 		if (GUI.Button (new Rect (Screen.width / 2 - 80, Screen.height / 2 + 200, 160, 40), "Main Menu"))
 		{
 			Application.LoadLevel ("Start_Menu");
-				
 		}
-			
 	}
-
 }
