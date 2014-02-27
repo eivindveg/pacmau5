c4393ca9678341a920bde6b51d2c68d2e51e37ed
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-02-27 17:43:56 +0100

    Create methods for advancing a level

---
+++ Assets/PillMonitor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class PillMonitor : MonoBehaviour
+{
+    private static List<GameObject> scorePills;
+
+    public static void EatPill(GameObject scorePill)
+    {
+        scorePills.Remove(scorePill);
+    }
+
+    // Use this for initialization
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+        scorePills = new List<GameObject>(GameObject.FindGameObjectsWithTag("ScorePill"));
+    }
+
+    // Update is called once per frame
+    // ReSharper disable once UnusedMember.Local
+    private void Update()
+    {
+        if (scorePills.Count <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+    }
+}
