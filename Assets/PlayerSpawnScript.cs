7d93fc7e9eb3e31735ebe68ccd8d06b7858a38de
Eivind Vegsundvåg <eivindveg@hotmail.com>
2014-02-27 18:32:27 +0100

    Improve teleporter, add PlayerSpawn behaviour.

---
+++ Assets/PlayerSpawnScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class PlayerSpawnScript : MonoBehaviour
+{
+    private GameObject player;
+
+    public void SpawnPlayer()
+    {
+        var spawnPosition = this.transform.position;
+        var newPlayer = (GameObject)Instantiate(Resources.Load("Actors/PlayerObject"));
+        this.player = newPlayer;
+        this.player.transform.position = spawnPosition;
+    }
+
+    // Use this for initialization
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+        this.SpawnPlayer();
+    }
+}
