33c450d5911c5f426f0a71486bf2977c854b13f4
RushTea <Eivindveg@hotmail.com>
2014-02-27 12:19:56 +0100

    Update and rename gameOverButtons.cs to GameOverButtons.cs

---
+++ Assets/GameOverButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverButtons : MonoBehaviour {
+
+
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2 - 80, Screen.height / 2 + 200, 160, 40), "Main Menu"))
+		{
+			Application.LoadLevel ("Start_Menu");
+				
+		}
+			
+	}
+
+}
